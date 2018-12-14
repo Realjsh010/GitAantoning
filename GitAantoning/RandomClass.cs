d6b72fc76e8beb74a48205595e51d0cbd416e544
Joris <jsh010gamestuff@hotmail.com>
2018-12-14 09:21:43 +0100

    RandomClass.cs Added

---
+++ GitAantoning/RandomClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitAantoning
+{
+    public class RandomClass
+    {
+        public int EenProperty { get; set; }
+
+        public RandomClass(int eenProperty)
+        {
+            EenProperty = eenProperty;
+        }
+    }
+}
